f7cbc2a6b3eaab22b89ac7a2ecdf366d7fd8c26e
Naruê Gonçalves Santos <naruegit@gmail.com>
2023-03-08 01:23:11 -0300

    feat: create the first version of the program

---
+++ main.py
@@ -0,0 +1,33 @@
+def count_notes(value, notes):
+  notes_count = []
+  for i in range(len(notes)):
+    note = value//notes[i]
+    value = value%notes[i]
+    notes_count.append(note)
+  return notes_count
+
+def run_program():
+  car = []
+  NOTES = [100, 50, 20, 10, 5, 2, 1]
+  while True:
+    product = {"name":"", "price":0}
+    name = input("Insira o nome do produto: ")
+    if not name: print("\nNome inválido\n")
+    else:
+      while True:
+        price = float(input("insira o valor do produto: "))
+        if not price or price < 0 or type(price) != float: print("Valor inválido")
+        else: product["name"] = name; product["price"] = price; break
+      car.append(product)
+      choice = input("Deseja inserir mais itens?(s/n): ")
+      if choice == "n": break
+  total = sum(list(map(lambda item: item['price'], car)))
+  print(f"total: {total}")
+  print(*list(map(lambda product: f"{product['name']} --- R${product['price']}\n", car)))
+  print("serão necessários: \n")
+  for i in range(len(NOTES)): 
+    if count_notes(total, NOTES)[i] > 0: print(f"{int(count_notes(total, NOTES)[i])} notas de {NOTES[i]}")
+
+
+
+run_program()
